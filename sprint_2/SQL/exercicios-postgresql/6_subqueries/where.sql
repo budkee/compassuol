8c8af160776c9c392f8fd8b4841b16252f36e9c2
budkee <camila.budke@gmail.com>
2023-08-08 16:17:54 -0400

    (08/08) SQL: Exercícios | Sprint 2

--- sprint_2/SQL/exercicios-postgresql/6_subqueries/where.sql
+++ sprint_2/SQL/exercicios-postgresql/6_subqueries/where.sql
@@ -8,4 +8,4 @@
 -- Informe qual é o veículo mais barato da tabela products
 select *
 from sales.products
-where price = (select min(price) from sales.products)
+where price = (select min(price) from sales.products);
--- sprint_2/SQL/exercicios-postgresql/6_subqueries/with.sql
+++ sprint_2/SQL/exercicios-postgresql/6_subqueries/with.sql
@@ -5,3 +5,15 @@
 
 -- Calcule a idade média dos clientes por status profissional
 
+with alguma_tabela as (
+select
+	professional_status,
+	(current_date - birth_date)/365 as idade
+from sales.customers
+)
+
+select
+	professional_status,
+	avg(idade) as idade_media
+from alguma_tabela
+group by professional_status
