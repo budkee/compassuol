c46bbb932bde2c54085349bcbee465d7569d8461
budkee <camila.budke@gmail.com>
2023-08-18 01:47:41 -0400

    (17/08) Exercícios Python | Sprint 3

--- sprint_03/python/exercicios/parte-2/ex16.py
+++ sprint_03/python/exercicios/parte-2/ex16.py
@@ -5,13 +5,19 @@ A string deve ter valor  "1,3,4,6,10,76"
 
 """
 # Declara função
-def receba(dados):
-    soma = 0
-    # Compute a soma
-    for _ in :
-        dados = int(dados)
-        soma += dados[_]
+def receba(string):
+    lista = string
+    separador = lista.split(',')
+    numeros_str = separador
+    numeros_tratados = []
+        ##print(numeros_tratados)
+        ##type(numeros_tratados[0])
+    for i in range(len(numeros_str)):
+        numeros_int = int(numeros_str[i])
+        numeros_tratados.append(numeros_int)
+    soma = sum(numeros_tratados)
     return soma
+
 
 # Início
 # Chame a função
