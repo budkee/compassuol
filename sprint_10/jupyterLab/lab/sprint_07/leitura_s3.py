e75ad01a5dcc43b7116a3290ecbbf0635199e892
budkee <kae.budke@gmail.com>
2024-04-26 14:26:30 -0400

    Organizando a documentação #01

--- sprint_10/jupyterLab/lab/sprint_07/leitura_s3.py
+++ sprint_10/jupyterLab/lab/sprint_07/leitura_s3.py
@@ -10,7 +10,7 @@ import boto3
 
 # Substitua 'ACCESS_KEY' e 'SECRET_KEY' pelas suas próprias credenciais AWS
 # s3 = boto3.client('s3', aws_access_key_id='ACCESS_KEY', aws_secret_access_key='SECRET_KEY')
-s3 = boto3.client('s3', aws_access_key_id='$YOUR_ACCESS_KEY', aws_secret_access_key='$YOUR_SECRET')
+s3 = boto3.client('s3', aws_access_key_id='$ACCESS_KEY', aws_secret_access_key='$SECRET_KEY')
 
 # Nome do bucket e nome do arquivo que você deseja ler
 bucket_name = 'tuckbe'
